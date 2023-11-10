c20f1952fd2697f49fc7f83a0d0c8cb6a158b037
Scott Ferguson <scottferg@outlook.com>
2023-11-09 21:14:16 -0800

    new FLogger

---
+++ FLog/FLogger.cpp
@@ -0,0 +1,36 @@
+#include "FLogger.h"
+
+FLogger FLogger::flog;
+flog_print_t FLogger::printer;
+
+int FLogger::flog_printfv(const char *format, va_list arg)
+{
+    static char loc_buf[64];
+    char * temp = loc_buf;
+    uint32_t len;
+    va_list copy;
+    va_copy(copy, arg);
+    len = vsnprintf(NULL, 0, format, copy);
+    va_end(copy);
+    if (len >= sizeof(loc_buf))
+    {
+        temp = (char*)malloc(len+1);
+        if(temp == NULL)
+            return 0;
+    }
+    vsnprintf(temp, len+1, format, arg);
+    (*printer)(temp);
+    if(len >= sizeof(loc_buf))
+        free(temp);
+    return len;
+}
+
+int FLogger::flog_printf(const char *format, ...)
+{
+    int len;
+    va_list arg;
+    va_start(arg, format);
+    len = flog_printfv(format, arg);
+    va_end(arg);
+    return len;
+}
---
+++ FLog/FLogger.h
@@ -0,0 +1,41 @@
+#ifndef FLogger_h
+#define FLogger_h
+
+#include <Arduino.h>
+
+#define FLOG_FORMAT(letter, format)  "[" #letter "] %s(): " format "\r\n", __FUNCTION__
+//#define FLOG_FORMAT(letter, format)  "[" #letter "][%s:%u] %s(): " format "\r\n", (unsigned long) pathToFileName(__FILE__), __LINE__, __FUNCTION__
+//#define FLOG_FORMAT(letter, format)  "[%6u][" #letter "][%s:%u] %s(): " format "\r\n", (unsigned long) (esp_timer_get_time() / 1000ULL), pathToFileName(__FILE__), __LINE__, __FUNCTION__
+
+#define flogd(format, ...) FLogger::flog.flog_printf(FLOG_FORMAT(D, format), ##__VA_ARGS__)
+
+typedef int (*flog_print_t)(const char *);
+
+class FLogger
+{
+protected:
+    static flog_print_t printer;
+
+    static int default_printer(const char* s)
+    {
+        return Serial.print(s);
+    }
+
+    FLogger() { printer = &default_printer; }
+
+
+public:
+    static FLogger flog;
+    static int flog_printfv(const char *format, va_list);
+    static int flog_printf(const char *format, ...);
+
+    static void SetPrinter(flog_print_t func)
+    {
+        printer = func;
+    }
+
+    FLogger(FLogger &other) = delete;           // not cloneable
+    void operator=(const FLogger &) = delete;   // not assignable
+};
+
+#endif // FLogger_h
